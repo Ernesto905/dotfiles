a9c3a80b7606e47117cd61d76a2ad173b2ef51f5
Ernesto Enriquez <44984106+Ernesto905@users.noreply.github.com>
2025-09-03 15:50:36 -0400

    Remove window navigation key mappings

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -10,11 +10,6 @@ vim.opt.showbreak = "↳ "
 vim.opt.textwidth = 80
 vim.opt.formatoptions:append("t")
 
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window', noremap = true, silent = true })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to lower window', noremap = true, silent = true })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to upper window', noremap = true, silent = true })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window', noremap = true, silent = true })
-
 vim.o.expandtab = true
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
